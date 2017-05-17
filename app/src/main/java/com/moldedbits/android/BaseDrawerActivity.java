20fb0a430caa819a128af88c475e551c3f566f66
Abhishek Bansal <discover.ab@gmail.com>
2017-05-17 14:21:11 +0530

    + we don’t need action bar menu in basedrawer activity

--- app/src/main/java/com/moldedbits/android/BaseDrawerActivity.java
+++ app/src/main/java/com/moldedbits/android/BaseDrawerActivity.java
@@ -49,28 +49,6 @@ public abstract class BaseDrawerActivity extends BaseActivity
         }
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.base_drawer, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
-
     @SuppressWarnings("StatementWithEmptyBody")
     @Override
     public boolean onNavigationItemSelected(@NonNull MenuItem item) {
